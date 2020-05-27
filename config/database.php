d0c0128b9b102d07586c80e4eb204adbe216a655
Leandro Rolim <leandro@viatechno.com.br>
2020-05-27 19:39:42 -0300

    config database

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-52-202-22-140.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'detkd0t6pdenp1'),
+            'username' => env('DB_USERNAME', 'mgfxpxrvalfslc'),
+            'password' => env('DB_PASSWORD', '2bf6ebdb824559a5eb6ca07a6f7dfb4cd0671e9df7a090f967c1eeacc61e3ba3'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
